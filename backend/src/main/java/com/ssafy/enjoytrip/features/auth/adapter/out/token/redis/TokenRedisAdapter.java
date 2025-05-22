82663571b5f35c1e182a7b1e1a13bf8b8507578f
한성민 <b12e34e56@naver.com>
2025-05-22 23:18:14 +0900

    SearchRefreshTokenPort 구현

--- backend/src/main/java/com/ssafy/enjoytrip/features/auth/adapter/out/token/redis/TokenRedisAdapter.java
+++ backend/src/main/java/com/ssafy/enjoytrip/features/auth/adapter/out/token/redis/TokenRedisAdapter.java
@@ -1,16 +1,18 @@
 package com.ssafy.enjoytrip.features.auth.adapter.out.token.redis;
 
 import com.ssafy.enjoytrip.features.auth.application.port.out.SaveRefreshTokenPort;
+import com.ssafy.enjoytrip.features.auth.application.port.out.SearchRefreshTokenPort;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
 
 import java.time.Duration;
+import java.util.Optional;
 
 @Component
 @RequiredArgsConstructor
-public class TokenRedisAdapter implements SaveRefreshTokenPort {
+public class TokenRedisAdapter implements SaveRefreshTokenPort, SearchRefreshTokenPort {
     @Value("${jwt.refresh-expmin}")
     private long refreshExpMin;
     private final StringRedisTemplate stringRedisTemplate;
@@ -18,5 +20,10 @@ public class TokenRedisAdapter implements SaveRefreshTokenPort {
     @Override
     public void saveRefreshToken(String accessToken, String refreshToken) {
         stringRedisTemplate.opsForValue().set(accessToken, refreshToken, Duration.ofMinutes(refreshExpMin));
+    }
+
+    @Override
+    public Optional<String> searchRefreshToken(String accessToken) {
+        return Optional.ofNullable(stringRedisTemplate.opsForValue().get(accessToken));
     }
 }
