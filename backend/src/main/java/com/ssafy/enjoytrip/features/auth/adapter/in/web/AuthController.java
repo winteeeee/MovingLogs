2a473c12206f6a859131987f3147c3600aadec7f
한성민 <b12e34e56@naver.com>
2025-05-23 00:14:31 +0900

    로그아웃 API 구현

--- backend/src/main/java/com/ssafy/enjoytrip/features/auth/adapter/in/web/AuthController.java
+++ backend/src/main/java/com/ssafy/enjoytrip/features/auth/adapter/in/web/AuthController.java
@@ -1,7 +1,13 @@
 package com.ssafy.enjoytrip.features.auth.adapter.in.web;
 
+import com.ssafy.enjoytrip.common.util.WebUtils;
+import com.ssafy.enjoytrip.features.auth.application.port.in.DeleteRefreshTokenUseCase;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,7 +16,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @Tag(name = "AuthController", description = "회원 인증 관련 API 제공")
 public class AuthController {
-    //TODO POST /refresh API 구현
-    //TODO POST /logout API 구현
+    private final DeleteRefreshTokenUseCase deleteRefreshTokenUseCase;
+
+    @PostMapping("/logout")
+    public ResponseEntity<Void> logout(HttpServletRequest request) {
+        String accessToken = WebUtils.extractToken(request);
+        DeleteRefreshTokenUseCase.Command command = new DeleteRefreshTokenUseCase.Command(accessToken);
+        deleteRefreshTokenUseCase.deleteRefreshToken(command);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
 }
 
