d073004125cb5a858aab071e48ea60d6d292d324
한성민 <b12e34e56@naver.com>
2025-05-22 15:17:01 +0900

    User 구현

--- backend/src/main/java/com/ssafy/enjoytrip/features/tripost/adapter/out/persistence/mybatis/dao/RouteSnapshotDao.java
+++ backend/src/main/java/com/ssafy/enjoytrip/features/tripost/adapter/out/persistence/mybatis/dao/RouteSnapshotDao.java
@@ -2,9 +2,11 @@ package com.ssafy.enjoytrip.features.tripost.adapter.out.persistence.mybatis.dao
 
 import com.ssafy.enjoytrip.features.tripost.domain.component.RouteSnapshot;
 import com.ssafy.enjoytrip.features.tripost.domain.TripostId;
+import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
 
+@Mapper
 public interface RouteSnapshotDao {
     int bulkUpsert(TripostId tripostId, List<RouteSnapshot> routeSnapshots);
     int deleteBySeqGreaterThan(TripostId tripostId, Integer seq);
--- backend/src/main/java/com/ssafy/enjoytrip/features/user/application/UserServiceMapper.java
+++ backend/src/main/java/com/ssafy/enjoytrip/features/user/application/UserServiceMapper.java
@@ -1,33 +1,10 @@
 package com.ssafy.enjoytrip.features.user.application;
 
-import com.ssafy.enjoytrip.features.user.application.port.in.CreateUserUseCase;
 import com.ssafy.enjoytrip.features.user.application.port.in.DeleteUserUseCase;
 import com.ssafy.enjoytrip.features.user.application.port.in.SearchUserUseCase;
-import com.ssafy.enjoytrip.features.user.application.port.in.UpdateUserUseCase;
 import com.ssafy.enjoytrip.features.user.domain.User;
 
 public class UserServiceMapper {
-    public static User toUser(CreateUserUseCase.Command command) {
-        User user = User.builder()
-                .id(command.getId())
-                .name(command.getName())
-                .activated(true)
-                .build();
-        user.activate();
-        user.setUserRole();
-        user.setCreateStatus();
-        return user;
-    }
-
-    public static User toUser(UpdateUserUseCase.Command command) {
-        User user = User.builder()
-                .id(command.getId())
-                .name(command.getName())
-                .build();
-        user.setUpdateStatus();
-        return user;
-    }
-
     public static User toUser(DeleteUserUseCase.Command command) {
         User user = User.builder()
                 .id(command.getId())
@@ -46,21 +23,9 @@ public class UserServiceMapper {
                 .build();
     }
 
-    public static CreateUserUseCase.Result toCreateUserResult(Boolean isCreated) {
-        return CreateUserUseCase.Result.builder()
-                .created(isCreated)
-                .build();
-    }
-
     public static DeleteUserUseCase.Result toDeleteUserResult(Boolean isDelete) {
         return DeleteUserUseCase.Result.builder()
                 .deleted(isDelete)
-                .build();
-    }
-
-    public static UpdateUserUseCase.Result toUpdateUserResult(Boolean isUpdated) {
-        return UpdateUserUseCase.Result.builder()
-                .updated(isUpdated)
                 .build();
     }
 }
