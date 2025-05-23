ddcdac5cd55dad17e838b5bef72d03d3304d70cd
한성민 <b12e34e56@naver.com>
2025-05-23 10:44:56 +0900

    updatePlan 테스트

--- backend/src/test/java/com/ssafy/enjoytrip/plan/PlanTest.java
+++ backend/src/test/java/com/ssafy/enjoytrip/plan/PlanTest.java
@@ -2,6 +2,7 @@ package com.ssafy.enjoytrip.plan;
 
 import com.ssafy.enjoytrip.features.plan.adapter.in.web.PlanController;
 import com.ssafy.enjoytrip.features.plan.adapter.in.web.request.CreatePlanRequest;
+import com.ssafy.enjoytrip.features.plan.adapter.in.web.request.UpdatePlanRequest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -32,5 +33,25 @@ public class PlanTest {
                 .build();
 
         planController.insertPlan(request);
+    }
+
+    @Test
+    void 계획_수정_테스트() {
+        List<String> attractions = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            attractions.add(String.valueOf(100000 + i));
+        }
+
+        UpdatePlanRequest request = UpdatePlanRequest.builder()
+                .id("0b41a106-cdc4-47e2-8d6f-b3d6a7570c87")
+                .title("업데이트된 테스트 제목2")
+                .desc("설명도 업데이트")
+                .thumbnailUrl(null)
+                .startDate(null)
+                .endDate(null)
+                .attractionIds(attractions)
+                .build();
+
+        planController.updatePlan(request);
     }
 }
