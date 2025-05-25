60c5de7cac34f13a875b281cbde7629c21cab0cd
백대환 <nm09090@naver.com>
2025-05-25 19:59:47 +0900

    Fix: image..adapter.out dao param어노테이션 누락 수정

--- backend/src/main/java/com/ssafy/enjoytrip/features/image/adapter/out/persistence/mybatis/dao/UploaderDao.java
+++ backend/src/main/java/com/ssafy/enjoytrip/features/image/adapter/out/persistence/mybatis/dao/UploaderDao.java
@@ -2,10 +2,11 @@ package com.ssafy.enjoytrip.features.image.adapter.out.persistence.mybatis.dao;
 
 import com.ssafy.enjoytrip.features.user.domain.Uid;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.Map;
 
 @Mapper
 public interface UploaderDao {
-    Map<String, Object> findById(Uid uid);
+    Map<String, Object> findById(@Param("uid") Uid uid);
 }
