67c46c53419bcab0b7c136ca089700f6f4d4fe10
백대환 <nm09090@naver.com>
2025-05-26 22:31:54 +0900

    Feat: 레이아웃 구분감을 위해, 헤더 그림자 더 짙게 수정

--- frontend/src/components/common/Header.vue
+++ frontend/src/components/common/Header.vue
@@ -124,7 +124,7 @@ function logout() {
 <style scoped>
 .site-header {
   background-color: white;
-  box-shadow: 0 2px 10px rgba(0, 0, 0, 0.05);
+  box-shadow: 0 2px 10px rgba(0, 0, 0, 0.12);
   position: sticky;
   top: 0;
   z-index: 100;
